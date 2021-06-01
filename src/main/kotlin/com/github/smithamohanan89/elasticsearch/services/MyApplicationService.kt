52c6b1b0c6ac23062450997f66334b04d3d45742
GitHub Action <action@github.com>
2021-06-01 10:56:46 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/smithamohanan89/elasticsearch/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.smithamohanan89.elasticsearch.services
+
+import com.github.smithamohanan89.elasticsearch.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/smithamohanan89/elasticsearch/services/MyProjectService.kt
+++ src/main/kotlin/com/github/smithamohanan89/elasticsearch/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.smithamohanan89.elasticsearch.services
 
+import com.github.smithamohanan89.elasticsearch.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
